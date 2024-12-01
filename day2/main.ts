41aef7b50b14a4c7c5b02e5e2d49bdc09b34ba45
fcalvez <fcalvez@gmail.com>
2024-12-01 16:47:01 -0500

    Prepare day2

---
+++ day2/main.ts
@@ -0,0 +1,6 @@
+import { extractNumbers } from "../utils.ts";
+import { readLines } from "../utils.ts";
+
+const lines = await readLines('day2\\input0.txt');
+console.log(`Found ${lines.length} lines`);
+
