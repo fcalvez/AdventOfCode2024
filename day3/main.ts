b59a9d2447283166a3ebdffe3067469be5627e3a
fcalvez <fcalvez@gmail.com>
2024-12-04 13:23:05 -0500

    day3.2

--- day3/main.ts
+++ day3/main.ts
@@ -3,18 +3,32 @@ import { readLines } from "../utils.ts";
 const lines = await readLines("day3\\input.txt");
 console.log(`Found ${lines.length} lines`);
 
-const regMemory = /mul\((\d{1,3}),(\d{1,3})\)/g;
+const regMemory = /(mul)\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don)'t\(\)/g;
 
 let total = 0;
-
+let enableMult = true;
 lines.forEach((line) => {
     let str;
     while ((str = regMemory.exec(line)) !== null) {
-        const n1 = Number(str[1]);
-        const n2 = Number(str[2]);
-        const mult = n1 * n2;
-        console.log(`${n1}x${n2}=${mult}`);
-        total += mult;
+        if (str[1] === "mul") {
+            const n1 = Number(str[2]);
+            const n2 = Number(str[3]);
+            const mult = n1 * n2;
+            if (enableMult) {
+                console.log(`${n1}x${n2}=${mult}`);
+                total += mult;
+            } else {
+                console.log(`ignored ${n1}x${n2}=${mult}`);
+            }
+        } else if (str[4] === "do") {
+            enableMult = true;
+            console.log("Do!");
+        } else if (str[5] === "don") {
+            enableMult = false;
+            console.log("Don't!");
+        } else {
+            console.log(`${str}`);
+        }
     }
 });
 
